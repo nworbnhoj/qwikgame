f1d1f12fc36faa8badd8049154d7027af27db601
nworbnhoj <nworbnhoj@gmail.com>
2017-04-22 21:29:13 -0300

    language translation files

---
+++ zh/lang.php
@@ -0,0 +1,190 @@
+<?php
+/* 
+------------------
+Language: Chinese
+------------------
+*/
+
+
+$GLOBALS['zh'] = array(); 
+$zh = &$GLOBALS['zh'];
+
+$zh['qwik_game_org'] = 'qwik game org';
+$zh['tagline'] = '找人玩你最喜欢的游戏，在一个适合你的时间和地点';
+$zh['license'] = "<a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'><img alt='知识共享许可协议' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/88x31.png' /></a><br />本作品由<a xmlns:cc='http://creativecommons.org/ns#' href='https://qwikgame.org' property='cc:attributionName' rel='cc:attributionURL'>qwikgame.org</a>创作，采用<a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。";
+$zh['Submit'] = '提交';
+$zh['Upload'] = 'Upload';
+$zh['Cancel'] = 'Cancel';
+$zh['Edit'] = 'Edit';
+$zh['link'] = 'link';
+$zh['homepage'] = 'homepage';
+$zh['Privacy_policy'] = '隐私权政策';
+
+$en['prompt_address'] = 'my.email@address.com';
+$zh['prompt_venue'] = '场地名称或郊区';
+
+
+
+$zh['Backgammon'] = '步步高';
+$zh['Badminton'] = '羽毛球';
+$zh['Boules'] = 'Boules';
+$zh['Billards'] = '台球';
+$zh['Checkers'] = '跳棋';
+$zh['Chess'] = '棋';
+$zh['Cycle'] = '自行车';
+$zh['Darts'] = '飞镖';
+$zh['Dirt Biking'] = 'Dirt Biking';
+$zh['Fly Fishing'] = 'Fly Fishing';
+$zh['Go'] = 'Go';
+$zh['Golf'] = 'Golf';
+$zh['Lawn Bowls'] = 'Lawn Bowls';
+$zh['Mountain Bike'] = 'Mountain Bike';
+$zh['Pool'] = 'Pool';
+$zh['Racquetball'] = 'Racquetball';
+$zh['Run'] = 'Run';
+$zh['Snooker'] = 'Snooker';
+$zh['Squash'] = '壁球';
+$zh['Table Tennis'] = '乒乓球';
+$zh['Tennis'] = 'Tennis';
+$zh['Tenpin'] = 'Tenpin';
+$zh['Walk'] = 'Walk';
+
+
+// index.html
+
+$zh['players_at_venues'] = "<b><v>playerCount</v></b> 名玩家在 <b><v>venueCount</v></b> <v>venuesLink</v><br>和计数";
+$zh['venues'] = '个场地';
+$zh['We_are_available...'] = '我们<b>可以</b>玩';
+$zh['I_am_keen...'] = '我喜欢在<br>下午五点<br>到八点玩游戏';
+$zh['I_am_A'] = '我在下午<br>五点是很<br>好的对手';
+$zh['I_am_B'] = '我更加<br>强大在下<br>午五点';
+$zh['I_am_C'] = '我在下i<br>午七点是<br>很好的对手';
+$zh['5pm'] = '下午五点';
+$zh['She_was...'] = '她更强<br>大但是';
+$zh['He_was...'] = '他更弱<br>小但是';
+$zh['Come_and_play...'] = '来和我们一起玩吧';
+$zh['When_a_rival...'] = 'When a rival is <b>keen</b> for a game you will be invited to play. Your email is never available to other players. You can also refine your availability once registered.';
+$zh['What_is_your...'] = '你最喜爱的游戏是什么';
+$zh['Let_me_know...'] = '让我知道当有人喜欢玩时<v>selectGame</v><v>inputVenue</v>';
+$zh['Venue_name...'] = '';
+$zh['matched_ability'] = '匹配的能力';
+$zh['Rate_the_ability...'] = '每场比赛后，帮助qwik游戏组织，估算与你水平相似的对手对手的能力。';
+$zh['You_can_estimate...'] = '你可以估计你自己的技能，以帮助你的前几场比赛。';
+$zh['polite_players'] = '文明玩家';
+$zh['Rate_the_politeness...'] = '每一场比赛后估算你的对手的素质，以建立他们的声誉。';
+$zh['You_can_ignore...'] = '你可以无视无礼的玩家';
+$zh['email_respect'] = '尊重电子邮件';
+$zh['Your_email_will_not...'] = '你的电子邮件将不会被其他的玩家看到也永远不会被分享或出售';
+$zh['You_will_receive...'] = '您将收到确认电子邮件；游戏邀请，确认与取消和反馈的要求。';
+$zh['your_data'] = '你的数据';
+$zh['You_can_quit...'] = '你可以在任何时候退出，在每一场比赛，只留下一个匿名的跟踪你的反馈
+（用于估计其他玩家的能力）';
+$zh[''] = '';
+$zh['spread_the_word'] = '传播这个词';
+$zh[''] = '';
+$zh[''] = '';
+$zh[''] = '';
+
+
+ 
+// Venue.html
+ 
+$zh['venuePageTitle'] = '<v>game</v> Venue';
+$zh['players_available'] = 'players available';
+$zh['Please_correct...'] = 'Please correct any details for this Venue.';
+$zh['You_can_easily_revert...'] = 'You can easily revert changes by clicking on the <s>struckout</s> data from prior edits.';
+$zh['timezone'] = 'timezone';
+$zh['Jump_Start'] = 'Jump Start';
+$zh['Here_are_a_few_things...'] = 'Here are a few things to jump start qwikgame.org at your favourite venue.';
+$zh['Make_sure_the_Venue...'] = 'Make sure the <b>Venue details</b> above are complete and correct.';
+$zh['Get_the_word_out'] = '<b>Get the word out.</b>';
+$zh['Encourage_your_rivals...'] = 'Encourage your rivals to submit when they are <b>available</b> to play.';
+$zh['emailShareSubject'] = 'qwikgame.org';
+$zh['emailShareBody'] = 'www.qwikgame.org%20makes%20it%20easy%20to%20find%20someone%20to%20play%20your%20favourite%20game%20at%20a%20time%20and%20place%20that%20suits%20you.';
+$zh['Put_this_link...'] = 'Put this <v>backLink</v> on the <v>venueName</v> <v>venueUrlLink</v> to make it easy for players to use this venue. ';
+$zh['Reckon_your_ability...'] = '<b>Reckon your ability</b> against the email address of your rivals, even if they are not using qwikgame.org yet.';
+$zh['Upload_a_list...'] = '<b>Upload</b> a list of player rankings to quickly improve the rival parity of qwik games.';
+
+// player.html
+
+$zh['Welcome'] = '欢迎';
+$zh['Hope_you_have...'] = '希望你最近有一些伟大的比赛。';
+$zh['Plaease_activate...'] = 'Please <b>activate</b> your account<br><br>An email has been sent with an activation link to click.';
+
+$zh['Matches'] = 'Matches';
+$zh['Available_rivals_have...'] = '<b>Available</b> rivals have been alerted that you are <b>keen</b>. The number of Open Invitations are those which have not yet been refused. Before too long, you should receive <b>invitations</b> to play.';
+
+$zh['Unfortunately_cancelled...'] = 'Unfortunately the match has been cancelled by your rival <v>rivalLink</v>';
+$zh['Cancelled_Game_at...'] = '<b><v>game</v></b> at <v>hour</v> <b><v>day</v></b> at <v>venueLink</v>.';
+$zh['Feedback_Game_at...'] = 'My <b><v>game</v></b> rival <v>rivalLink</v> at <v>hour</v> <v>day</v> at <v>venueLink</v> was a ';
+$zh['player'] = 'player';
+$zh['Confirmed_Game_at...'] = '<b><v>game</v></b> at <v>hour</v> <b><v>day</v></b> at <v>venueLink</v> with a <v>parity</v> player <v>rivalRep</v>, <v>rivalLink</v>.';
+$zh['Accepted_Game_at...'] = '<b><v>game</v></b> at <v>hour</v> <b><v>day</v></b> at <v>venueLink</v> with a <v>parity</v> player <v>rivalRep</v>, <v>rivalLink</v>.';
+$zh['Invitation_Game_at...'] = '<b><v>game</v></b> at <v>hour</v> <b><v>day</v></b> at <v>venueLink</v> with a <v>parity</v> player <v>rivalRep</v>, <v>rivalLink</v>.';
+$zh['Keen_Game_at...'] = '<b><v>game</v></b> at <v>hour</v> <b><v>day</v></b> at <v>venueLink</v>.';
+
+$zh['potential_rivals'] = 'potential rivals';
+$zh['This_will_alert...'] = 'This will alert the rivals who are <b>available</b> that you are keen for a game. Before too long you should receive a few invitations to play.';
+$zh[''] = '';
+$zh['at'] = 'at';
+$zh['am'] = '早上';
+$zh['pm'] = '下午';
+$zh['today_or_tomorrow'] = '今天i<br>或明天';
+$zh['invite_familiar_rivals'] = '邀请熟悉的对手';
+$zh['invite_available_rivals'] = '邀请可用的对手';
+
+$zh['Available'] = 'Available?'; 
+$zh['Make_yourself_available...'] = 'Make yourself <b>available</b> to play at the times and venues that suit your weekly routine. When a rival is <b>keen</b> for a game you will be invited to play.';
+$zh['Available_Game_with...'] = '<b><v>game</v></b> with <b><v>parity</v></b> players at <v>venueLink</v>';
+$zh['Let_me_know_when...'] = '<b>Let me know</b> when someone of';
+$zh['matching'] = 'matching';
+$zh['similar'] = 'similar';
+$zh['any'] = 'any';
+$zh['ability_is_keen...'] = 'ability is <b>keen</b> to play';
+$zh['anytime'] = 'anytime';
+
+$zh['Familiar'] = 'Familiar';
+$zh['Familiar_players_help...'] = 'Familiar players help to establish your ability;<br>and you can invite familiar players when you are keen for a game.';
+$zh['is_a_familiar'] = '<v>email</v> is a <v>parity</v> <v>game</v> player.';
+$zh['This_is_an_effective...'] = 'This is an effective way to help qwikgame.org to estimate your parity with your known rivals. Even if your known rival is not (yet) using qwikgame.org, anonymous player rankings may have been uploaded which include them.<br>This will not be visible to other players.';
+$zh['Your_rivals_will_not...'] = 'Your rivals will NOT receive unsolicited email or invitations from qwikgame.org. Please encourage them to join!';
+$zh['email_is_familiar'] = '<v>email</v> is a <v>parity</v> <b><v>game</v></b> player';
+$zh['is_a'] = 'is a';
+$zh['much_stronger'] = 'much stronger';
+$zh['stronger'] = 'stronger';
+$zh['well_matched'] = 'well matched';
+$zh['weaker'] = 'weaker';
+$zh['much_weaker'] = 'much weaker';
+$zh['player'] = 'player';
+
+$zh['Ability'] = 'Ability';
+$zh['Reckon_your_own...'] = 'Reckon your own ability to improve your first few qwik games.';
+$zh['parity_game_player...'] = '<v>ability</v> <b><v>game</v></b> player in <v>region</v>';
+$zh['Be_as_specific...'] = 'Be as specific and accurate as you can be. This is not visible to other players and is only used to setup your first few matches. Once you have provided feedback on your first few matches qwikgame.org will be better able to estimate your ability for better parity with your rivals.';
+$zh['I_am_a'] = 'I am a';
+$zh['very_strong'] = 'very strong';
+$zh['strong'] = 'strong';
+$zh['competent'] = 'competent';
+$zh['weak'] = 'weak';
+$zh['very_weak'] = 'very weak';
+$zh['player_in'] = 'player in';
+
+
+$zh['History'] = 'History';
+$zh['You_can_disown...'] = 'You can disown your match feedback so that it becomes an anonymous record of your rivals ability and politeness.';
+$zh['History_Game_at...'] = "My <v>game</v> rival <v>rivalLink</v> at <v>hour</v> <v>day</v> at <v>venueLink</v> was a <b><v>parity</v></b> <button type='button' class='<v>thumb</v>'></button> player.";
+
+$zh['Acount'] = 'Account';
+$zh['public'] = 'public';
+$zh['Your_nickname_will...'] = 'Your <b>nick-name</b> will be visible to other players in match invitations and the like. You can also provide the url of your favourite social networking page.';
+$zh['You_have_a...'] = 'You have a <v>reputation</v> <v>reputationLink</v>';
+$zh['nickname'] = 'nickname';
+$zh['private'] = 'private';
+$zh['You_will_receive_validation...'] = 'You will receive an email containing a validation link at your updated email address. Your old email address will be replaced by the new. Your old email address will continue to exist in system backup files until they become redundant and are deleted.<br>Your <b>email</b> address will <b>not be visible</b> to other players, and will <b>never be willingly shared or sold</b>';
+$zh['Your_anonymous_match...'] = 'Your anonymous match feedback history will remain on the system to help grade the skill and politeness of other players. Your email address will continue to exist in system backup files until they become redundant and are deleted. You will receive an account recovery email containing the ONLY remaining link between your email address and your match feedback.';
+$zh['Quit_qwikgame.org'] = 'Quit qwikgame.org';
+$zh['This_will_immediately...'] = 'This will immediately remove your email address from qwikgame.org and anonymise your match feedback history.';
+$zh['Quit'] = 'Quit';
+
+
+?>

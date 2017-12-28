16468648ebaea459128e0ab5df0050cbc8143774
nworbnhoj <nworbnhoj@gmail.com>
2017-12-27 21:32:40 -0300

    Implement UploadPage Class

---
+++ class/UploadPage.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once 'Page.php';
+
+
+class UploadPage extends Page {
+
+    public function __construct($template='upload'){
+        parent::__construct($template);
+    }
+	
+	
+    public function processRequest(){
+
+        $player = $this->player;
+        $qwik = $this->req('qwik');
+        $game = $this->req('game');
+        $title = $this->req('title');
+        $filename = $this->req('filename');
+
+	if (isset($player) && isset($qwik)){
+            $qwik = $this->req('qwik');
+
+            switch ($qwik) {
+                case 'upload':
+                    if(isset($game) && isset($title)){
+                        $player->rankingUpload($game, $title);
+                    }
+	            break;
+                case "activate":
+                    if(isset($filename)){
+                        $player->rankingActivate($filename);
+                    }
+                    break;
+                case 'deactivate':
+                    if(isset($filename)){
+                        $player->rankingDeactivate($filename);
+                    }
+                    break;
+                case 'delete':
+                    if(isset($filename)){
+                        $player->rankingDelete($filename);
+                    }
+                    break;
+            }
+            $player->save();
+        }
+    }
+    
+    
+    public function variables(){
+        $variables = parent::variables();
+        $player = $this->player;
+
+        $variables['please_login']   = $player ? '' : '{prompt_login}';
+        $variables['uploadHidden']   = $player ? '' : 'hidden';
+        $variables['uploadDisabled'] = $player ? '' : 'disabled';
+        $variables['TICK_ICON']      = self::TICK_ICON;
+        $variables['LOGOUT_ICON']    = isset($player) ? self::LOGOUT_ICON : '';
+
+        return $variables;
+    }
+    
+}
+
+?>
--- upload.php
+++ upload.php
@@ -1,66 +1,6 @@
 <?php
-    require 'qwik.php';
+require_once 'class/UploadPage.php';
 
-    $req = validate($_POST);
-    if (!$req){
-        $req = validate($_GET);
-    }
-
-    logReq($req);
-    $player = login($req);
-	$lang = language($req, $player);
-
-
-	if ($player && isset($req['qwik'])){
-		$qwik = $req['qwik'];
-
-        switch ($qwik) {
-            case 'upload':
-				if(isset($req['game'])
-			    && isset($req['title'])){
-			        $game = $req['game'];
-			        $title = $req['title'];
-			        $player->rankingUpload($game, $title);
-				}
-	            break;
-            case "activate":
-                if(isset($req['filename'])){
-					$fileName = $req['filename'];
-					$player->rankingActivate($fileName);
-				}
-    	        break;
-			case 'deactivate':
-                if(isset($req['filename'])){
-                    $fileName = $req['filename'];
-					$player->rankingDeactivate($fileName);
-                }
-				break;
-            case 'delete':
-                if(isset($req['filename'])){
-                    $fileName = $req['filename'];
-                    $player->rankingDelete($fileName);
-                }
-            	break;
-		}
-		$player->save();
-	}
-
-
-    $variables = array(
-		'please_login'	=> $player ? '' : '{prompt_login}',
-    	'uploadHidden'	=> $player ? '' : 'hidden',
-		'uploadDisabled'	=> $player ? '' : 'disabled',
-        'INFO_ICON'     => $INFO_ICON,
-        'HOME_ICON'     => $HOME_ICON,
-		'TICK_ICON'		=> $TICK_ICON,
-        'CROSS_ICON'    => $CROSS_ICON,
-        'RELOAD_ICON'   => $RELOAD_ICON,
-        'LOGOUT_ICON'   => isset($player) ? $LOGOUT_ICON : '',
-    );
-
-    $html = file_get_contents("lang/$lang/upload.html");
-    $html = replicate($html, $player);
-    $html = populate($html, $variables);
-    $html = translate($html, $lang);
-    echo ($html);
+$page = new UploadPage();
+$page->serve();
 ?>

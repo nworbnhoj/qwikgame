be5135d57af76ed45dec3e204e05d492f2c48155
nworbnhoj <nworbnhoj@gmail.com>
2018-04-02 06:27:35 -0300

    move geolocation functions from VenuePage to LocatePage

--- class/LocatePage.php
+++ class/LocatePage.php
@@ -7,6 +7,16 @@ require_once 'VenuePage.php';
 
 class LocatePage extends Page {
 
+
+    const GEOPLACE_URL   = "https://maps.googleapis.com/maps/api/place/autocomplete/xml";
+    const GEODETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/xml";
+    const GEOCODE_URL    = "https://maps.googleapis.com/maps/api/geocode/xml";
+
+    const GEOPLACE_API_KEY   = "AIzaSyDne6EhcdFtiEiUT-batwVilT9YFUAbYdM";
+    const GEODETAILS_API_KEY = "AIzaSyDne6EhcdFtiEiUT-batwVilT9YFUAbYdM";
+    const GEOCODE_API_KEY    = "AIzaSyC4zcdOxikM54AHNQjcSLSd8d6N8Kebmfg";
+
+
     private $game;
     private $description;
     private $repost;
@@ -66,7 +76,7 @@ class LocatePage extends Page {
             && isset($admin1)
             && isset($country)){
                 $description = "$name, $locality, $admin1, $country";
-                $placeid = VenuePage::getPlace($description);
+                $placeid = self::getPlace($description);
                 if(isset($placeid)){
                     $vid = $this->newVenue($placeid, $name);
                 } else {
@@ -87,7 +97,7 @@ class LocatePage extends Page {
     
     private function newVenue($placeid, $reqName){
         $vid = NULL;
-        $address = VenuePage::getDetails($placeid);
+        $address = self::getDetails($placeid);
 
         if($address){
             $vid = Venue::venueID(
@@ -143,7 +153,7 @@ class LocatePage extends Page {
 
         if (empty($name)){
             $name = $this->description;
-            $geocoded = VenuePage::parseAddress($this->description);
+            $geocoded = self::parseAddress($this->description);
             if($geocoded){
                 $locality = $geocoded['locality'];
                 $admin1 = $geocoded['admin1'];
@@ -165,6 +175,109 @@ class LocatePage extends Page {
         $variables['datalists']      = $this->countryDataList();
         
         return $variables;
+    }
+
+
+    static function geo($param, $key, $url){
+        $result = null;
+        $param['key'] = $key;
+        $query = http_build_query($param);
+        $contents = file_get_contents("$url?$query");
+        $xml = new SimpleXMLElement($contents);
+        $status = (string) $xml->status[0];
+        if($status === 'OK'){
+            $result = $xml->result;
+        } else {
+            $msg = $xml->error_message;
+            self::logMsg("Google $status: $msg\n\t$url?$query");
+        }
+        return $result;
+    }
+
+
+    static function geoplace($text){
+        return self::geo(
+            array('input'=>$text),
+            self::GEOPLACE_API_KEY,
+            self::GEOPLACE_URL
+        );
+    }
+
+
+    static function geodetails($placeid){
+        return self::geo(
+            array('placeid'=>$placeid),
+            self::GEODETAILS_API_KEY,
+            self::GEODETAILS_URL
+        );
+    }
+
+
+    static function geocode($address, $country){
+        return self::geo(
+            array('components'=>"$address|$country"),
+            self::GEOCODE_API_KEY,
+            self::GEOCODE_URL
+        );
+    }
+
+
+    static function getPlace($description){
+        $placeid = NULL;
+        $xml = self::geoplace($description);
+        if(isset($xml)){
+            $prediction = $xml->prediction[0];
+            if(isset($prediction)){
+                $placeid = (string) $prediction->place_id;
+            }
+        }
+        return $placeid;
+    }
+
+
+    static function getDetails($placeid){
+        $details = array();
+        $xml = self::geodetails($placeid);
+        if(isset($xml)){
+            $details['placeid'] = $placeid;
+
+            $result = $xml->result;
+            $details['formatted'] = (string) $result->formatted_address;
+
+            $location = $result->geometry->location;
+            $details['lat'] = (string) $location->lat;
+            $details['lng'] = (string) $location->lng;
+
+            $addr = $result->xpath("address_component[type='country']")[0];
+            $details['country'] = (string) $addr->long_name;
+            $details['country_code'] = (string) $addr->short_name;
+
+            $addr = $result->xpath("address_component[type='administrative_area_level_1']")[0];
+            $details['admin1'] = (string) $addr->long_name;
+            $details['admin1_code'] = (string) $addr->short_name;
+
+            $addr = $result->xpath("address_component[type='administrative_area_level_2']")[0];
+            $details['admin2'] = (string) $addr->long_name;
+
+            $addr = $result->xpath("address_component[type='administrative_area_level_3']")[0];
+            $details['admin3'] = (string) $addr->long_name;
+
+            $addr = $result->xpath("address_component[type='locality']")[0];
+            $details['locality'] = (string) $addr->long_name;
+
+            $details['phone'] = (string) $result->phone[0];            $details['url'] = (string) $result->website[0];
+        }
+        return $details;
+    }
+
+
+    static function parseAddress($address){
+        $parsed = FALSE;
+        $placeid = self::getPlace($address);
+        if (isset($placeid)){
+            $parsed = self::getDetails($placeid);
+        }
+        return $parsed;
     }
     
 }
--- class/VenuePage.php
+++ class/VenuePage.php
@@ -1,36 +1,9 @@
 <?php
 
 require_once 'Page.php';
+require_once 'LocatePage.php';
 
 class VenuePage extends Page {
-
-    const GEOPLACE_URL   = "https://maps.googleapis.com/maps/api/place/autocomplete/xml";
-    const GEODETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/xml";
-    const GEOCODE_URL    = "https://maps.googleapis.com/maps/api/geocode/xml";
-
-    const GEOPLACE_API_KEY   = "AIzaSyDne6EhcdFtiEiUT-batwVilT9YFUAbYdM";
-    const GEODETAILS_API_KEY = "AIzaSyDne6EhcdFtiEiUT-batwVilT9YFUAbYdM";
-    const GEOCODE_API_KEY    = "AIzaSyC4zcdOxikM54AHNQjcSLSd8d6N8Kebmfg";
-
-    const PDR_XPATH_STATUS       = "/PlaceDetailsResponse/status/text()";
-    const PDR_XPATH_ERROR        = "/PlaceDetailsResponse/error_message/text()";
-    const PDR_XPATH_FORMATTED    = "/PlaceDetailsResponse/result/formatted_address/text()";
-    const PDR_XPATH_COUNTRY      = "/PlaceDetailsResponse/result/address_component[type='country']/long_name/text()";
-    const PDR_XPATH_COUNTRY_CODE = "/PlaceDetailsResponse/result/address_component[type='country']/short_name/text()";
-    const PDR_XPATH_ADMIN1       = "/PlaceDetailsResponse/result/address_component[type='administrative_area_level_1']/long_name/text()";
-    const PDR_XPATH_ADMIN1_CODE  = "/PlaceDetailsResponse/result/address_component[type='administrative_area_level_1']/short_name/text()";
-    const PDR_XPATH_ADMIN2       = "/PlaceDetailsResponse/result/address_component[type='administrative_area_level_2']/long_name/text()";
-    const PDR_XPATH_ADMIN3       = "/PlaceDetailsResponse/result/address_component[type='administrative_area_level_3']/long_name/text()";
-    const PDR_XPATH_LOCALITY     = "/PlaceDetailsResponse/result/address_component[type='locality']/long_name/text()";
-    const PDR_XPATH_PHONE        = "/PlaceDetailsResponse/result/address_component[type='phone']/text()";
-    const PDR_XPATH_URL          = "/PlaceDetailsResponse/result/address_component[type='url']/text()";
-    const PDR_XPATH_LAT          = "/PlaceDetailsResponse/result/geometry/location/lat/text()";
-    const PDR_XPATH_LNG          = "/PlaceDetailsResponse/result/geometry/location/lng/text()";
-
-    const ACR_XPATH_STATUS     = "/AutocompletionResponse/status/text()";
-    const ACR_XPATH_ERROR      = "/AutocompletionResponse/error_message/text()";
-    const ACR_XPATH_PREDICTION = "/AutocompletionResponse/prediction";
-
 
     private $venue;
 
@@ -61,7 +34,7 @@ class VenuePage extends Page {
         && $req('name') !== null
         && $req('address') !== null
         && $req('country') !== null){
-            $address = self::parseAddress($req['address'].', '.$req['country']);
+            $address = LocatePage::parseAddress($req['address'].', '.$req['country']);
 
             $save = $venue->updateAtt('name',     $req['name']);
             $save = $venue->updateAtt('locality', $address['locality'])  || $save;
@@ -127,114 +100,6 @@ class VenuePage extends Page {
         
         return $vars;
     }
-
-
-    static function geo($param, $key, $url){
-        $result = null;
-        $param['key'] = $key;
-        $query = http_build_query($param);
-        $contents = file_get_contents("$url?$query");
-        $xml = new SimpleXMLElement($contents);
-        $status = (string) $xml->status[0];
-        if($status === 'OK'){
-            $result = $xml->result;
-        } else {
-            $msg = $xml->error_message;
-            self::logMsg("Google $status: $msg\n\t$url?$query");
-        }
-        return $result;
-    }
-
-
-
-
-    static function geoplace($text){
-        return self::geo(
-            array('input'=>$text),
-            self::GEOPLACE_API_KEY,
-            self::GEOPLACE_URL
-        );
-    }
-
-
-    static function geodetails($placeid){
-        return self::geo(
-            array('placeid'=>$placeid),
-            self::GEODETAILS_API_KEY,
-            self::GEODETAILS_URL
-        );
-    }
-
-
-    static function geocode($address, $country){
-        return self::geo(
-            array('components'=>"$address|$country"),
-            self::GEOCODE_API_KEY,
-            self::GEOCODE_URL
-        );
-    }
-
-
-    static function getPlace($description){
-        $placeid = NULL;
-        $xml = self::geoplace($description);
-        if(isset($xml)){
-            $prediction = $xml->prediction[0];
-            if(isset($prediction)){
-                $placeid = (string) $prediction->place_id;
-            }
-        }
-        return $placeid;
-    }
-
-
-
-    static function getDetails($placeid){
-        $details = array();
-        $xml = self::geodetails($placeid);
-        if(isset($xml)){
-            $details['placeid'] = $placeid;
-
-            $result = $xml->result;
-            $details['formatted'] = (string) $result->formatted_address;
-
-            $location = $result->geometry->location;
-            $details['lat'] = (string) $location->lat;
-            $details['lng'] = (string) $location->lng;
-
-            $addr = $result->xpath("address_component[type='country']")[0];
-            $details['country'] = (string) $addr->long_name;
-            $details['country_code'] = (string) $addr->short_name;
-
-            $addr = $result->xpath("address_component[type='administrative_area_level_1']")[0];
-            $details['admin1'] = (string) $addr->long_name;
-            $details['admin1_code'] = (string) $addr->short_name;
-
-            $addr = $result->xpath("address_component[type='administrative_area_level_2']")[0];
-            $details['admin2'] = (string) $addr->long_name;
-
-            $addr = $result->xpath("address_component[type='administrative_area_level_3']")[0];
-            $details['admin3'] = (string) $addr->long_name;
-
-            $addr = $result->xpath("address_component[type='locality']")[0];
-            $details['locality'] = (string) $addr->long_name;
-
-            $details['phone'] = (string) $result->phone[0];            $details['url'] = (string) $result->website[0];
-        }
-        return $details;
-    }
-
-
-
-    static function parseAddress($address){
-        $parsed = FALSE;
-        $placeid = self::getPlace($address);
-        if (isset($placeid)){
-            $parsed = getDetails($placeid);
-        }
-        return $parsed;
-    }
-
 
 }
 

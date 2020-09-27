421c01dc3917da9e256c11ee8837aed8db4dcd8b
nworbnhoj <nworbnhoj@gmail.com>
2020-09-28 09:53:50 +1000

    chmod 664 .xml

--- class/Qwik.php
+++ class/Qwik.php
@@ -584,6 +584,10 @@ class Qwik {
             throw new RuntimeException("failed to save xml to $path$fileName from cwd $cwd");
             return FALSE;
         }
+        if(!chmod($fileName, 0664)){
+            throw new RuntimeException("failed to chmod 0664 $path$fileName");
+            return FALSE;
+        }
         if(!chdir($cwd)){
             throw new RuntimeException("failed to return working directory to $cwd");
             return FALSE;

b4cfcd350962488735da053b8fe6eae4dd51ffc5
nworbnhoj <john@nhoj.info>
2023-02-04 13:04:47 +1100

    re-implement permissions & build notes

---
+++ host/permissions.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo -n "Setting file permissions"
+
+cd "$(git rev-parse --show-toplevel)" ; echo -n "."
+
+chown -R www-admin:www *              ; echo -n "."
+chmod -R u+rwX *                      ; echo -n "."
+chmod -R go-rwx *                     ; echo -n "."
+
+chmod -R g+rX  class                  ; echo -n "."
+chmod -R g+rwX delayed                ; echo -n "."
+chmod -R g+rX  html                   ; echo -n "."
+chmod -R g+rwX lang                   ; echo -n "."
+chmod -R g+rwX mark                   ; echo -n "."
+chmod -R g+rwX player                 ; echo -n "."
+chmod -R g+rwX uploads                ; echo -n "."
+chmod -R g+rX  vendor                 ; echo -n "."
+chmod -R g+rwX venue                  ; echo -n "."
+chmod -R g+rX  www                    ; echo -n "."
+
+chmod    g+r   path.php               ; echo -n "."
+chmod    g+r   resubmit.php           ; echo -n "."
+chmod    g+r   services.xml           ; echo -n "."
+
+touch /var/log/beta.qwikgame.org.log
+chown www-admin:www /var/log/beta.qwikgame.org.log
+chmod 660 /var/log/beta.qwikgame.org.log
+
+echo "."

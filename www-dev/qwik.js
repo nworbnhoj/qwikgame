bce7b5f37d7c0de41d40bfce56d2ad1197a7f255
nworbnhoj <john@nhoj.info>
2024-01-23 10:35:40 +1100

    sketch keen invite form

--- www-dev/qwik.js
+++ www-dev/qwik.js
@@ -1,3 +1,29 @@
+function enableElement(element, enable) {
+  if (enable) {
+    element.classList.remove('disabled');
+    element.querySelectorAll('input').forEach(function(input){
+      input.disabled = false;
+    });
+    element.querySelectorAll('.disabled').forEach(function(disabled){
+      disabled.classList.remove('disabled');
+    });
+  } else {
+    element.classList.add('disabled');
+    element.querySelectorAll('input').forEach(function(input){
+      input.disabled = true;
+    });
+    element.querySelectorAll('h6, div').forEach(function(enabled){
+      enabled.classList.add('disabled');
+    });
+  }
+}
+
+function enableInviteFriend(event) {
+  let element = event.currentTarget;
+  enableElement(document.getElementById("invite_friend_body"), element.checked);
+  enableElement(document.getElementById("rival_skill_body"), !element.checked);
+}
+
 function href(event) {
     let element = event.currentTarget;
     window.location.href = element.dataset.href;
@@ -42,7 +68,7 @@ function showDetail() {
     detail.style.display = "flex";    
   }
 
-  [].forEach.call(document.querySelectorAll('[name=list]'), function(radio){
+  list_bar.querySelectorAll('[name=list]').forEach(function(radio){
     if (radio.checked) {
           document.getElementById(radio.dataset.id).classList.remove("hidden");
       } else {
@@ -78,11 +104,50 @@ function showGroup(event) {
     button.lastElementChild.classList.toggle('flip');
 }
 
+function showNextSibling(event) {
+  let button = event.currentTarget;
+  let next_sibling = button.nextElementSibling;
+  next_sibling.classList.toggle('hidden');
+}
+
+function showNextParentSibling(event) {
+  let button = event.currentTarget;
+  let next_sibling = button.parentNode.nextElementSibling;
+  next_sibling.classList.toggle('hidden');
+}
+
+function toggleAllDay(event) {
+  let button = event.currentTarget;
+  var checked = button.firstElementChild.checked;
+  var hours = button.closest('.schedule_keen').querySelector('.radio_block').children;
+  for (hour of hours) {
+    var checkbox = hour.firstElementChild;
+    if (!checkbox.disabled) {
+      checkbox.checked = checked;
+    }
+  }
+}
+
 window.onload = function() {
-  [].forEach.call(document.querySelectorAll('[name=list]'), function(list_radio) {
-    list_radio.onclick = showDetail;
+  document.querySelectorAll('[name=list]').forEach(function(list_radio) {
+      list_radio.onclick = showDetail;
   });
   document.querySelectorAll('button.show-group').forEach(function(button){
       button.onclick = showGroup;
+  });
+  document.querySelectorAll('button.show-next-sibling').forEach(function(button){
+      button.onclick = showNextParentSibling;
+  });
+  document.querySelectorAll('button.show-next-parent-sibling').forEach(function(button){
+      button.onclick = showNextParentSibling;
+  });
+  document.querySelectorAll('.href').forEach(function(element){
+      element.onclick = href;
+  });
+  document.querySelectorAll('label.toggle.button.all-day').forEach(function(button){
+      button.onclick = toggleAllDay;
+  });
+  document.querySelectorAll('input.enable-invite-friend').forEach(function(checkbox){
+      checkbox.oninput = enableInviteFriend;
   });
 }

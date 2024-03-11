8eb8bcd74fb8812d9ce259162a7030939bff8043
nworbnhoj <john@nhoj.info>
2024-03-12 06:21:22 +1000

    init static

--- qwikgame/settings.py
+++ qwikgame/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "qwikgame/static"
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

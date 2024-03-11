a83bc78623ee7cea91fd2b266ebc5421364f1a02
nworbnhoj <john@nhoj.info>
2024-03-12 08:55:44 +1000

    add authenticate User.language

--- authenticate/models.py
+++ authenticate/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
+LANGUAGE = [
+    ('bg', 'български'),
+    ('en', 'English'),
+    ('es', 'Español'),
+    ('zh', '中文'),
+    ('ru', 'русский'),
+    ('fr', 'Français'),
+    ('hi', 'हिंदी'),
+    ('ar', 'اللغة العربية'),
+    ('jp', '日本語'),
+]
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         """
@@ -37,13 +49,14 @@ class User(PermissionsMixin, AbstractBaseUser):
         max_length=255,
         unique=True,
     )
+    language = models.CharField(max_length=2, choices=LANGUAGE, default='en',)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["language"]
 
     def __str__(self):
         return self.email

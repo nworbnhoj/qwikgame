7d178dbe6b1c56da24ef724108a117f70c83e2b1
nworbnhoj <john@nhoj.info>
2024-03-12 14:16:34 +1000

    init match app

--- qwikgame/settings.py
+++ qwikgame/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "authenticate.apps.AuthenticateConfig",
     'game.apps.GameConfig',
+    'match.apps.MatchConfig',
     'player.apps.PlayerConfig',
     'venue.apps.VenueConfig',
     'django.contrib.admin',

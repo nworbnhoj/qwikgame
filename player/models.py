050fb1f116f9f19d1b97b7551d7d2f420a3ee81a
nworbnhoj <john@nhoj.info>
2024-03-12 14:04:02 +1000

    add ManyToManyField(Game)

--- player/models.py
+++ player/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 from authenticate.models import User
+from game.models import Game
 
 
 class Player(models.Model):
     email_hash = models.CharField(max_length=32, primary_key=True)
+    games = models.ManyToManyField(Game)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- venue/models.py
+++ venue/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from game.models import Game
+
 
 class Venue(models.Model):
+    games = models.ManyToManyField(Game)
     name = models.CharField(max_length=128)
     url = models.URLField(max_length=256, blank=True)
 

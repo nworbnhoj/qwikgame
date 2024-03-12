870f20106273e4c998194dc17760937a748c4214
nworbnhoj <john@nhoj.info>
2024-03-13 09:39:14 +1000

    include models in admin

--- player/admin.py
+++ player/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Player
+from .models import Appeal, Available, Friend, Invite, Opinion, Player, Precis, Strength
 
+admin.site.register(Appeal)
+admin.site.register(Available)
+admin.site.register(Friend)
+admin.site.register(Invite)
+admin.site.register(Opinion)
 admin.site.register(Player)
+admin.site.register(Precis)
+admin.site.register(Strength)
--- venue/admin.py
+++ venue/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Venue
+from .models import Manager, Venue
 
+admin.site.register(Manager)
 admin.site.register(Venue)

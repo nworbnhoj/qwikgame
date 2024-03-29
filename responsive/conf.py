55139b621a31078f2affdc6c24e22b6e1f1f38d8
nworbnhoj <john@nhoj.info>
2024-03-29 10:51:17 +1000

    vendor in django-responsive2 0.1.3
    https://github.com/mishbahr/django-responsive2

---
+++ responsive/conf.py
@@ -0,0 +1,73 @@
+from django.conf import settings  # noqa
+from django.utils.translation import ugettext_lazy as _
+
+from appconf import AppConf
+
+
+class ResponsiveAppConf(AppConf):
+    """
+    While there are several different items we can query on,
+    the ones used for django-responsive2 are min-width, max-width, min-height and max-height.
+
+    min_width -- Rules applied for any device width over the value defined in the config.
+    max_width -- Rules applied for any device width under the value defined in the config.
+    min_height -- Rules applied for any device height over the value defined in the config.
+    max_height -- Rules applied for any device height under the value defined in the config.
+    pixel_ratio -- Rules applied for any device with devicePixelRatio defined in the config.
+
+    Usage
+    ------
+        {% load 'responsive' %}
+
+        {% renderblockif 'small' 'medium' %}
+            [...]
+        {% endrenderblockif %}
+
+    """
+
+    COOKIE_NAME = 'clientinfo'
+    COOKIE_AGE = 365  # days
+
+    CACHE_PREFIX = 'responsive_'
+    CACHE_DURATION = 60 * 60 * 24 * 356  # 1 year
+
+    VARIABLE_NAME = 'device'
+    DEFAULT_WIDTH = 0
+    DEFAULT_HEIGHT = 0
+    DEFAULT_PIXEL_RATIO = 1
+
+    # Borrowed from ZURB Foundation framework.
+    # See http://foundation.zurb.com/docs/media-queries.html
+    MEDIA_QUERIES = {
+        'small': {
+            'verbose_name': _('Small screens'),
+            'min_width': None,
+            'max_width': 640,
+        },
+        'medium': {
+            'verbose_name': _('Medium screens'),
+            'min_width': 641,
+            'max_width': 1024,
+        },
+        'large': {
+            'verbose_name': _('Large screens'),
+            'min_width': 1025,
+            'max_width': 1440,
+        },
+        'xlarge': {
+            'verbose_name': _('XLarge screens'),
+            'min_width': 1441,
+            'max_width': 1920,
+        },
+        'xxlarge': {
+            'verbose_name': _('XXLarge screens'),
+            'min_width': 1921,
+            'max_width': None,
+        },
+        'retina': {
+            'pixel_ratio': 2
+        }
+    }
+
+    class Meta:
+        prefix = 'responsive'
---
+++ responsive/middleware.py
@@ -0,0 +1,66 @@
+from __future__ import unicode_literals
+
+import datetime
+import re
+
+from django.template.loader import render_to_string
+from django.utils.cache import patch_vary_headers
+from django.utils.encoding import smart_bytes
+
+
+from .conf import settings
+from .utils import Device
+
+
+class ResponsiveMiddleware(object):
+
+    def process_request(self, request):
+        responsive_cookie = request.COOKIES.get(settings.RESPONSIVE_COOKIE_NAME, None)
+        if responsive_cookie:
+            parts = responsive_cookie.split(':')
+            if len(parts) != 3:
+                request.INVALID_RESPONSIVE_COOKIE = True
+                return
+
+            try:
+                width, height, pixel_ratio = parts
+                width, height, pixel_ratio = int(width), int(height), float(pixel_ratio)
+            except ValueError:
+                request.INVALID_RESPONSIVE_COOKIE = True
+                return
+
+            device_info = {
+                'width': width,
+                'height': height,
+                'pixel_ratio': pixel_ratio
+            }
+
+            device = Device(**device_info)
+            setattr(request, settings.RESPONSIVE_VARIABLE_NAME, device)
+
+    def process_response(self, request, response):
+        html_types = ('text/html', 'application/xhtml+xml')
+        content_type = response.get('Content-Type', '').split(';')[0]
+        content_encoding = response.get('Content-Encoding', '')
+        if any((getattr(response, 'streaming', False),
+                'gzip' in content_encoding,
+                content_type not in html_types)):
+            return response
+
+        if settings.RESPONSIVE_COOKIE_NAME not in request.COOKIES \
+                or getattr(request, 'INVALID_RESPONSIVE_COOKIE', False):
+            expires = datetime.datetime.utcnow() + \
+                datetime.timedelta(days=settings.RESPONSIVE_COOKIE_AGE)
+            snippet = render_to_string('responsive/snippet.html', {
+                'cookie_name': settings.RESPONSIVE_COOKIE_NAME,
+                'cookie_age': 60 * 60 * 24 * settings.RESPONSIVE_COOKIE_AGE,  # convert to secs
+                'cookie_expires': expires.strftime('%a, %d %b %Y %H:%M:%S GMT')
+            })
+            pattern = re.compile(b'<head()>|<head (.*?)>', re.IGNORECASE)
+            response.content = pattern.sub(b'<head\g<1>>' + smart_bytes(snippet), response.content)
+
+            if response.get('Content-Length', None):
+                response['Content-Length'] = len(response.content)
+
+        patch_vary_headers(response, ('Cookie', ))
+        return response

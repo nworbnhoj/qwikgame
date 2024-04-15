ed942e4a6e87284e052fd39fe82041dd20324e50
nworbnhoj <john@nhoj.info>
2024-04-16 04:20:12 +1000

    implement Game block list

--- game/views.py
+++ game/views.py
@@ -12,7 +12,13 @@ class GameView(QwikView):
 
     def get(self, request, *args, **kwargs):
         super().request_init(request)
-        context = super().context(request)
+        player = self.user.player
+        available = Available.objects.filter(player=player)
+        context = {
+            'games': player.games.all(),
+            'available': available.all()
+        }
+        context |= super().context(request)
         if context['small_screen']:
             return render(request, "game/game.html", context)
         else:
@@ -25,8 +31,11 @@ class ActiveView(QwikView):
 
     def get(self, request, *args, **kwargs):
         super().get(request)
-        context = self.active_form_class.get(self.user.player)
-        context = context | super().context(request)
+        player = self.user.player
+        available = Available.objects.filter(player=player)
+        context = {'available': available.all()}
+        context |= self.active_form_class.get(player)
+        context |= super().context(request)
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
@@ -34,7 +43,7 @@ class ActiveView(QwikView):
         context = self.active_form_class.post(request.POST, self.user.player)
         if len(context) == 0:
             return HttpResponseRedirect("/game/")
-        context = context | super().context(request)
+        context |= super().context(request)
         return render(request, self.template_name, context)
 
 
@@ -46,31 +55,34 @@ class AvailableView(QwikView):
     def get(self, request, *args, **kwargs):
         super().get(request)
         player, game, venue, hours = self.user.player, None, None, None
+        available = Available.objects.filter(player=player)
+        context = {'available': available.all()}
         game_name = kwargs.pop('game', None)
         venue_name = kwargs.pop('venue', None)
         if game_name is not None:
             game = get_object_or_404(Game, name=game_name)
-            context = { 'game': game, 'game_name': game.name }
+            context |= { 'game': game, 'game_name': game.name }
         if venue_name is not None:
             venue = get_object_or_404(Venue, name=venue_name)
-            context = context | { 'venue': venue, 'venue_name': venue.name }
+            context |= { 'venue': venue, 'venue_name': venue.name }
+        context |= {'hide_view': 'hidden'}
         if (player is not None and game is not None and venue is not None):
             try:
                 available = Available.objects.get(player=player, game=game, venue=venue)
                 hours: bytearray = available.hours
                 context['blocks'] = AvailableView.hour_blocks(available.get_hours_week(), range(6,21))
                 context['hide_edit'] = 'hidden'
+                context['hide_view'] = ''
             except:
-                context['hide_view'] = 'hidden'
-             
-        context = context | self.available_form_class.get(
+                pass             
+        context |= self.available_form_class.get(
             player,
             game=game.pk if game is not None else None,
             hide=self.hide,
             venue=venue.pk if venue is not None else None,
             hours=hours,
         )
-        context = context | super().context(request)
+        context |= super().context(request)
         return render(request, self.template_name, context)
 
     def hour_blocks(week, range):
@@ -108,7 +120,7 @@ class AvailableView(QwikView):
         )
         if len(context) == 0:
             return HttpResponseRedirect("/game/")
-        context = context | super().context(request)
+        context |= super().context(request)
 
         game, venue = None, None
         game_name = kwargs.pop('game', None)
@@ -117,6 +129,6 @@ class AvailableView(QwikView):
             game = get_object_or_404(Game, name=game_name)
         if venue_name is not None:
             venue = get_object_or_404(Venue, name=venue_name)
-        context = context | {'game': game, 'game_name': game.name }
+        context |= {'game': game, 'game_name': game.name }
 
         return render(request, self.template_name, context)

217f7f38bb9045874ec0e56cd6de1d8c836e80bb
nworbnhoj <john@nhoj.info>
2024-06-20 09:47:20 +0300

    configure secret_key environment variable

--- qwikgame/settings.py
+++ qwikgame/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%%ow*cyif)h7op61xa6&pk+fk0r*$(n&fu*oy)6h!j-uh+cry4'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','error: set environ variable DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

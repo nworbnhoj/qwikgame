e48a1549900772b6c73177228199523c33b601b4
nworbnhoj <john@nhoj.info>
2024-12-03 12:27:11 +0700

    add Entry.created and display in chat_bubble

--- qwikgame/log.py
+++ qwikgame/log.py
@@ -1,4 +1,6 @@
+from django.utils.timezone import now
+
 class Entry(dict):
 
     def __init__(self, id, icon=None, klass=None, name=None, text=None):
-        dict.__init__(self, id=id, icon=icon, klass=klass, name=name, text=text)
+        dict.__init__(self, id=id, created=now().strftime("%Y-%m-%d %H:%M:%S%z"), icon=icon, klass=klass, name=name, text=text)

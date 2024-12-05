c89963364bfe1ee9114487a8c3499269cf466be3
nworbnhoj <john@nhoj.info>
2024-12-05 09:11:07 +0700

    implement AppealForm Submit wording change on Friends visibility

--- qwikgame/static/keen.js
+++ qwikgame/static/keen.js
@@ -7,7 +7,32 @@ docReady(event => {
 });
 
 
-winReady(event => {});
+winReady(event => {
+  // https://stackoverflow.com/questions/1462138/event-listener-for-when-element-becomes-visible
+  respondToVisibility = function(element, callback) {
+    var options = { root: document.documentElement }
+    var observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        callback(entry.intersectionRatio > 0);
+      });
+    }, options);
+    observer.observe(element);
+  }
+
+  const friends_field = document.getElementById("id_friends").closest("div.field")
+  respondToVisibility(friends_field, visible => {
+    const submit = document.getElementById("appeal_submit");
+    if(visible) {
+      submit.querySelector(".appeal_all").hidden = true;
+      submit.querySelector(".appeal_friends").hidden = false;
+     } else {
+      submit.querySelector(".appeal_all").hidden = false;
+      submit.querySelector(".appeal_friends").hidden = true;
+     }
+  });
+
+
+});
 
 
 function initPage(){

nworbnhoj/qwikgame

commit cdcaf8c4fc53b47091356f02d89248d6d9e16b53

remove django div.html override

qwikgame/static/css/qwik.css
@@ -114,7 +114,7 @@ label.btn.reveal { display: inline-block; }
 .todo { display: none; border: 3px dotted SkyBlue }
 a.btn {position: relative;}
 body { align-items: center; display: flex; flex-direction: column; font-family: NotoSans }
-div { align-items: center; display: flex; }
+div { align-items: start; display: flex; flex-direction: column; }
 div.banner {background-color: #ffffff; flex: none; flex-direction: column; position: -webkit-sticky; position: sticky; top: 4em; margin: 0 -1em 0 -1em; padding: 1em; text-align: center }
 div.banner.active { background-color: #dfa622; }
 div.banner.complete { background-color: lightgrey; }
@@ -174,7 +174,7 @@ div.icon.medium { height: 3em; width: 3em }
 div.icon.small { border-width: 0.2em; height: 2em; width: 2em }
 div.icon.tiny { border-width: 0.1em; height: 1em; width: 1em }
 div.icon_bar { background-color: white; border: 0px solid black; display: flex; flex-direction: row; gap: 1em; height: 4em; justify-content: space-around; left: 0; opacity: 1.0; position: fixed; top: 0; width: 100% }
-div.icon_bar_reserve { height: 4em }
+div.icon_bar_reserve { height: 4.4em }
 div.info { position: relative }
 div.info .info_text { background-color: white; border: 1px solid black; border-radius: 0.3em; font-size: 0.8em; justify-content: center; opacity: 1; padding: 0.3em; position: absolute; right: -0.2em; text-align: right; top: 150%; width: 20em; z-index: 2 }
 div.info .info_text::after { border-color: transparent transparent black transparent; border-width: 0.8em; bottom: 100%; content: " "; position: absolute; right: 0; margin-left: -0.8em; border-style: solid }
@@ -190,10 +190,10 @@ div.log_entry.match {}
 div.option {align-items: center; justify-content: space-between; }
 div.options { align-items: stretch; background-color: white; border: 1px solid #bbbbbb; border-radius: 0.3em; flex-direction: column; justify-content: flex-start; opacity: 1.0; padding: 0.3em; position: relative; width: 100% }
 div.paver { flex: auto; flex-direction: row; gap: 2em; justify-content: center; padding: 1em; }
-div.pillar { flex: auto; flex-direction: column; justify-content: start; padding: 1em }
+div.pillar { align-items: center; flex: auto; flex-direction: column; justify-content: start; padding: 1em }
 div.plank { flex: auto; flex-direction: row; gap: 2em; justify-content: start; padding: 1em; width: 100% }
 div.pole { align-items: start; flex: auto; flex-direction: column; justify-content: start; width: 100% }
-div.range_options { gap: 1em; justify-content: space-between; padding: 1em 0 0 0 ; width: 100% }
+div.range_options { flex-direction: row; gap: 1em; justify-content: space-between; padding: 1em 0 0 0 ; width: 100% }
 div.range_options .option { text-align: center }
 div.replicate { border: 1px solid black; margin: 1em 0 1em 0; padding: 1em 0 1em 2em; }
 div.post { align-items: stretch; flex-direction: column; justify-content: start; width: 100% }

templates/django/forms/div.html
@@ -1,15 +1,0 @@
-{{ errors }}
-{% if errors and not fields %}
-  <div>{% for field in hidden_fields %}{{ field }}{% endfor %}</div>
-{% endif %}
-{% for field, errors in fields %}
-  <div {% with classes=field.css_classes %}{% if classes %} class="{{ classes }} field"{% else %} class="field"{% endif %}{% endwith %}>
-    {{ field.as_field_group }}
-    {% if forloop.last %}
-      {% for field in hidden_fields %}{{ field }}{% endfor %}
-    {% endif %}
-</div>
-{% endfor %}
-{% if not fields and not errors %}
-  {% for field in hidden_fields %}{{ field }}{% endfor %}
-{% endif %}

templates/dropdown.html
@@ -1,10 +1,12 @@
 <!-- qwikgame/templates/dropdown.html -->
+<div class="field">
 {% include 'field_label.html' %}
 {{ field.errors }}
-<div class="drop_down">
-    <div class="btn drop" data-down="false">
-        <div class="drop_up" data-action="{{ field.field.action }}">{{ language }}</div>
-        <div class="fat">v</div>
+    <div class="drop_down">
+        <div class="btn drop" data-down="false">
+            <div class="drop_up" data-action="{{ field.field.action }}">{{ language }}</div>
+            <div class="fat">v</div>
+        </div>
+{{ field }}
     </div>
-    {{ field }}
 </div>

templates/field.html
@@ -1,9 +1,11 @@
 <!-- qwikgame/templates/field.html -->
 {% if field.use_fieldset %}
+    <fieldset>
     {% if field.label %}
         {{ field.legend_tag }}
     {% endif %}
 {% else %}
+    <div class="field">
     {% if field.label %}
         {% include 'field_label.html' %}
     {% endif %}
@@ -19,4 +21,7 @@
 {{ field }}
 
 {% if field.use_fieldset %}
+    </fieldset>
+{% else %}
+    </div>
 {% endif %}

commit 251f6fbd54f1f1d77cc7bd2f31d55b0df4378009

fix numerous css issues
caused mostly by cdcaf8c4fc53b47091356f02d89248d6d9e16b53

player/templates/player/account_public_snippet.html
@@ -7,15 +7,7 @@
                         </div>
                     </div>
                     <div class="plank">
-{% for _ in '12345'|make_list %} 
-                        <div class="icon color">
-{% if forloop.counter <= player.conduct_stars %}
-                            <i class="fa-solid fa-star fa-2x"></i>
-{% else %}
-                            <i class="fa-regular fa-star fa-2x"></i>
-{% endif %}
-                        </div>
-{% endfor %}
+{% include 'player/conduct_stars_snippet.html' %}
                     </div>
                     {{ precis_form }}
                     {{ player_form }}

player/templates/player/conduct_stars_snippet.html
@@ -1,9 +1,9 @@
 <!-- qwikgame/player/templates/player/conduct_stars_snippet.html -->
 
-    <div >
+    <div class="plank" >
 {% for _ in '12345'|make_list %} 
         <div class="icon color">
-{% if forloop.counter <= rival.conduct_stars %}
+{% if forloop.counter <= player.conduct_stars %}
             <i class="fa-solid fa-star fa-1x"></i>
 {% else %}
             <i class="fa-regular fa-star fa-1x"></i>

player/templates/player/friend.html
@@ -82,26 +82,6 @@
                 </div>
             </form>
         </div>
-        <div id="KEEN" class="post hidden">
-        <hr><hr>
-            <div class="post detail_body">
-                <h1>GAME</h1>
-                <h3>STRENGTH</h3>
-                <div class="slide_labels">
-                    <div class="slide_label">Much Weaker</div>
-                    <div class="slide_label">Weaker</div>
-                    <div class="slide_label">Well Matched</div>
-                    <div class="slide_label">Stronger</div>
-                    <div class="slide_label">Much Stronger</div>
-                </div>
-                <input class="strength" type="range" min="0" max="4" value="2" oninput="slide(this)">
-                <div class="rival-skill-learn grid_3_col">
-                    <div class="btn tertiary show_friend_invite">Cancel</div>
-                    <div class="btn tertiary">Learn more</div>
-                    <div class="btn secondary">Add</div>
-                </div>
-            </div>            
-        </div>
     </div>
 {% endblock %}
 

player/templates/player/rival_header_snippet.html
@@ -9,7 +9,9 @@
             </div>
         </div>
         <div class="pillar align_center">    
-{% include 'player/conduct_stars_snippet.html' %}
+{% with rival as player %}
+    {% include 'player/conduct_stars_snippet.html' %}
+{% endwith %}
             <div>
                 <p>{{ strength }}</p>
             </div>

qwikgame/static/css/qwik.css
@@ -66,7 +66,7 @@ label.btn.reveal { display: inline-block; }
 
 .btn.large { font-size: 1.13em; }
 .btn.loader { color: transparent; }
-.btn.small { font-size: 0.88em; min-height: 2em;}
+.btn.small { font-size: 0.88em; height: 1.2em;}
 .btn.square { aspect-ratio: 1 / 1; }
 .btn.round { aspect-ratio: 1 / 1; border-radius: 50%; }
 
@@ -118,7 +118,7 @@ label.btn.reveal { display: inline-block; }
 .todo { display: none; border: 3px dotted SkyBlue }
 a.btn {position: relative;}
 body { align-items: center; display: flex; flex-direction: column; font-family: NotoSans }
-div { align-items: start; display: flex; flex-direction: column; }
+div { }
 div.banner {background-color: #ffffff; flex: none; flex-direction: column; position: -webkit-sticky; position: sticky; top: 4em; margin: 0 -1em 0 -1em; padding: 1em; text-align: center }
 div.banner.active { background-color: #dfa622; }
 div.banner.complete { background-color: lightgrey; }
@@ -130,10 +130,10 @@ div.appeal_item { display: grid; grid-auto-flow: column; grid-template-columns: 
 div.chat_bubble { display: grid; background-color: #eeeeee;  column-gap: 1em; grid-template-columns: auto auto; grid-template-rows: 2em auto;  padding: 0.3em; }
 div.chat_bubble .name { font-weight: bold; }
 div.chat_bubble .text { grid-column: 1/-1; }
-div.chat_input { flex-direction: row; gap: 0.3em; justify-content: stretch; width: 100% }
+div.chat_input { align-items: center; display: flex; flex-direction: row; gap: 0.3em; justify-content: stretch; width: 100% }
 div.chat_item { border: 0 solid black; flex: auto; flex-direction: row; gap: 0.3em; justify-content: flex-start; margin: 0em; max-width: 66%; padding: 0.3em }
 div.chat_item .name { font-weight: bold; }
-div.content { align-items: stretch; flex-direction: row; justify-content: stretch; max-width: 1200px; width: 100% }
+div.content { align-items: stretch; display:flex; flex-direction: row; justify-content: stretch; max-width: 1200px; width: 100% }
 div.day { flex: 0 0 auto; flex-grow: 4; height: 2em; white-space: nowrap }
 div.day.fri { grid-column: 1 / 1; grid-row: 5 / 5 }
 div.day.mon { grid-column: 1 / 1; grid-row: 1 / 1 }
@@ -145,11 +145,11 @@ div.day.wed { grid-column: 1 / 1; grid-row: 3 / 3 }
 div.detail { align-items: stretch; display: flex; flex: 0 1 68.75%; flex-direction: column; gap: 1em; padding: 0 1em 10em 1em }
 div.detail_body { }
 div.detail_form { }
-div.detail_head { align-items: center; border-style: solid; border-width: 0 0 0.1em 0; flex: 0 0 auto; flex-direction: row; gap: 1em; justify-content: start; margin: 0 -1em 0 -1em; padding: 1em }
+div.detail_head { align-items: center; border-style: solid; border-width: 0 0 0.1em 0; display: flex; flex: 0 0 auto; flex-direction: row; gap: 1em; justify-content: start; margin: 0 -1em 0 -1em; padding: 1em }
 div.detail_n { flex: 0 0 auto; gap: 1em; }
 div.detail_rival { flex-direction: row; flex-wrap: wrap; justify-content: center; width: 100% }
-div.down { align-items: start; background-color: white; border: 1px solid #888888; flex-direction: column; gap: 0.5em; left: 0; opacity: 1.0; padding: 1em; position: absolute; top: 3.2em; white-space: nowrap; width: 100%; z-index: 10 }
-div.down div { justify-content: space-between; width:100% }
+div.down { align-items: start; background-color: white; border: 1px solid #888888; display: flex; flex-direction: column; gap: 0.5em; left: 0; margin: 0.3em; opacity: 1.0; padding: 1em; position: absolute; top: 3.2em; white-space: nowrap; width: 100%; z-index: 10 }
+div.down div { justify-content: space-between; padding: 0.3em; width:100% }
 div.down div.link { justify-content: end; width: auto }
 div.down label { align-items: stretch; gap: 1em; justify-content: space-between; padding: 0.3em 0 0.3em 0; text-align: center }
 div.down label:has(input:checked) { color: #dfa622 }
@@ -167,7 +167,7 @@ div.flex_space { flex-grow: 3 }
 div.grid_2_col { display: grid; grid-template-columns: 1fr 1fr; gap: 1em; justify-items: center; padding: 1em 0 0 0; width: 100% }
 div.grid_3_col { display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 1em; justify-items: center; margin: 0.3em; padding: 1em 0 0 0; width: 100% }
 div.head { display: grid; grid-auto-flow: column; grid-template-columns: 2em auto; grid-template-rows: 1fr 1fr 1fr; justify-items: start; width: 100% }
-div.hidden { display: none }
+
 div.hour_grid { grid-template-columns: repeat(auto-fit, minmax(3em, max-content)); justify-content: center; width: 100% }
 div.hrs { border: 1px solid black; border-radius: 0.3em; height: 2em; justify-content: center }
 div.hrs.apm { background-color: #eeeeee; border: 0px solid black; color: white; flex: auto; font-size: xx-large; justify-content: center }
@@ -183,24 +183,24 @@ div.info { position: relative }
 div.info .info_text { background-color: white; border: 1px solid black; border-radius: 0.3em; font-size: 0.8em; justify-content: center; opacity: 1; padding: 0.3em; position: absolute; right: -0.2em; text-align: right; top: 150%; width: 20em; z-index: 2 }
 div.info .info_text::after { border-color: transparent transparent black transparent; border-width: 0.8em; bottom: 100%; content: " "; position: absolute; right: 0; margin-left: -0.8em; border-style: solid }
 div.info_bubble { align-items: center; background-color: #eeeeee; justify-content: flex-start; margin: 0.3em; padding: 0.3em }
-div.label { flex: auto; flex-direction: row; gap: 2em; justify-content: start; padding: 1em 0 0.5em 0; width: 100% }
+div.label { align-items: center; display: flex; flex: auto; flex-direction: row; gap: 2em; justify-content: start; padding: 1em 0 0.5em 0; width: 100% }
 div.list { align-items: stretch; border: 0.1em solid black; border-width: 0 0.1em 0 0; display: flex; flex: 1 1 100%; flex-direction: column; justify-content: flex-start; overflow: hidden; padding: 0 1em 1em 1em; width: 100% }
-div.list_bar { align-items: start; flex: 0 1 auto; justify-content: start; width: 31.25%; }
-div.list_head { align-items: center; border: 0.1em solid black; border-width: 0 0 0.1em 0; flex: 0 0 auto; flex-direction: row; height: 4em; justify-content: space-between; margin: 0 -1em 0 -1em; padding: 1em }
-div.log_entry { border: 0 solid black; flex: auto; flex-direction: row; gap: 0.3em; margin: 0em; max-width: 66%; padding: 0.3em }
+div.list_bar { align-items: start; display: flex; flex: 0 1 auto; justify-content: start; width: 31.25%; }
+div.list_head { align-items: center; border: 0.1em solid black; border-width: 0 0 0.1em 0; display: flex; flex: 0 0 auto; flex-direction: row; height: 4em; justify-content: space-between; margin: 0 -1em 0 -1em; padding: 1em }
+div.log_entry { border: 0 solid black; display: flex; flex: auto; flex-direction: row; gap: 0.3em; margin: 0em; padding: 0.3em }
 div.log_entry.chat {}
 div.log_entry.event {}
 div.log_entry.match {}
 div.option {align-items: center; justify-content: space-between; }
 div.options { align-items: stretch; background-color: white; border: 1px solid #bbbbbb; border-radius: 0.3em; flex-direction: column; justify-content: flex-start; opacity: 1.0; padding: 0.3em; position: relative; width: 100% }
 div.paver { flex: auto; flex-direction: row; gap: 2em; justify-content: center; padding: 1em; }
-div.pillar { align-items: center; flex: auto; flex-direction: column; justify-content: start; padding: 1em }
-div.plank { flex: auto; flex-direction: row; gap: 2em; justify-content: start; padding: 1em; width: 100% }
+div.pillar { align-items: center; display: flex; flex: auto; flex-direction: column; justify-content: start; padding: 1em }
+div.plank { display: flex; flex: auto; flex-direction: row; justify-content: start; padding: 1em; width: 100% }
 div.pole { align-items: start; flex: auto; flex-direction: column; justify-content: start; width: 100% }
-div.range_options { flex-direction: row; gap: 1em; justify-content: space-between; padding: 1em 0 0 0 ; width: 100% }
+div.range_options { display: flex; flex-direction: row; gap: 1em; justify-content: space-between; padding: 1em 0 0 0 ; width: 100% }
 div.range_options .option { text-align: center }
 div.replicate { border: 1px solid black; margin: 1em 0 1em 0; padding: 1em 0 1em 2em; }
-div.post { align-items: stretch; flex-direction: column; justify-content: start; width: 100% }
+div.post { align-items: stretch; display: flex; flex-direction: column; justify-content: start; width: 100% }
 div.radio_block { display: grid; gap: 1em; margin: 0 0 1em 0 }
 div.rival_rate_grid { grid-template-columns: 1fr 1fr; flex-direction: row }
 div.schedule { display: grid; flex: 1 1 100%; gap: 0.3em; grid-template-columns: repeat(17, 1fr); grid-template-rows: repeat(7, auto); justify-items: stretch; width: 100% }
@@ -220,7 +220,8 @@ div.qwikgame { align-items: center; display: flex; flex-direction: row; font-siz
 div.slide_labels { gap: 1em; justify-content: space-between; width: 100% }
 div.slide_label { text-align: center }
 div.upgrade { border: 1px solid black; padding: 1em }
-fieldset { background-color: inherit; border: 1px solid #cccccc; display: flex; flex-direction: column; margin: 1em 0 1em 0; padding: 2em }
+div.hidden { display: none }
+fieldset { background-color: inherit; border: 1px solid #cccccc; display: flex; flex-direction: column; margin: 1em 0 1em 0; padding: 2em; width: 100%; }
 fieldset.disabled { background-color: #eeeeee; border: 0px solid #eeeeee; color: #bbbbbb }
 fieldset div { display:block; padding: 1em}
 h1 { align-items: center; color: black; display: flex; flex-direction: row; font-size: 1.13em; justify-content: start; text-align: left; margin: 0.3em; width: 100% }

qwikgame/widgets.py
@@ -83,6 +83,7 @@ class DayInputRadio(RadioSelect):
 
     def __init__(self, attrs=None, choices=()):
         super().__init__(attrs, choices)
+        self.use_fieldset=False
 
     def set_data_attr(self, key, value):
         self.data_attrs[f'data-{key}', value]
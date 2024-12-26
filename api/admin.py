8cb350e0532b6f2fa15282c873486b34ac9f83f6
nworbnhoj <john@nhoj.info>
2024-12-27 04:08:32 +1100

    refine MarkAdmin list_display

--- api/admin.py
+++ api/admin.py
@@ -4,6 +4,7 @@ from .models import Mark
 
 
 class MarkAdmin(admin.ModelAdmin):
+    list_display = ['game', 'place', 'num_player', 'num_venue']
     list_filter = ['game', 'place__country', 'place__admin1', 'place__locality']
     ordering = ['game', 'place__country', 'place__admin1', 'place__locality']
 

nworbnhoj/qwikgame

commit bab12eca1e515bf1b01639fe03b1f22085c37be3

persist Appeal until end of day

player/views.py
@@ -111,11 +111,8 @@ class AcceptView(AppealsView):
                 game = appeal.game
                 venue = appeal.venue
                 appeal.meta['seen'] = [player.pk]
-                # mark this Appeal seen by this Player only
-                appeal.meta['seen'] = [player.pk]
-                appeal.save()
+                appeal.log_event('cancelled')
                 logger.info(f'Cancelling Appeal: {appeal}')
-                appeal.delete()
                 # update the Mark size
                 mark = Mark.objects.filter(game=game, place=venue).first()
                 if mark:
@@ -129,7 +126,6 @@ class AcceptView(AppealsView):
                 bid = Bid.objects.get(pk=accept_pk)
                 bid.log_event('accept')
                 match = Match.from_bid(bid)
-                bid.appeal.delete()
                 match.log_event('scheduled')
                 match.clear_conflicts()
                 return HttpResponseRedirect(f'/game/match/{match.id}/')

player/models.py
@@ -477,11 +477,13 @@ class Appeal(models.Model):
                     self.venue.datetime(self.date).strftime("%b %d"),
                     self.hours24.as_str()
                 )
+            case 'cancelled':
+                entry['text'] = "Cancelled Invitation"
             case _:
                 logger.warn(f'unknown template: {template}')
         self.log_entry(entry)
 
-    # Deletes the Appeal when all hours have passed.
+    # Deletes the Appeal at the end of the day.
     def perish(self, dry_run=False):
         action = 'noop'
         now = self.venue.now()
@@ -489,15 +491,6 @@ class Appeal(models.Model):
             if not dry_run:
                 self.delete()
             action = 'expired'
-        elif now.date() == self.date:
-            hour = now.hour
-            past =  [False for h in range(0, hour+1)]
-            future = [True for h in range(hour+1, 24)]
-            hours24 = Hours24(self.hours) & Hours24(past + future)
-            if hours24.is_none():
-                if not dry_run:
-                    self.delete()
-                action = 'expired'
         logger.debug('Appeal{} {: <9} {} @ {} {}'.format(
                 ' (dry-run)' if dry_run else '',
                 action,

commit 7b9dbe84ed775c039d6f787dd42119bc1058a4ba

fix bid.delete() after calling Match.from_bid()
bug introduced by: bab12eca1e515bf1b01639fe03b1f22085c37be3
bid was previously deleted by appeal.delete()

resolves #83

player/views.py
@@ -130,8 +130,9 @@ class AcceptView(AppealsView):
                 appeal.alert(player)
                 appeal.save()
                 bid = Bid.objects.get(pk=accept_pk)
-                bid.log_event('accept')
                 match = Match.from_bid(bid)
+                bid.log_event('accept')
+                bid.delete()
                 match.alert(player)
                 match.log_event('scheduled')
                 match.clear_conflicts(appeal)
@@ -203,7 +204,8 @@ class BidView(AppealsView):
                 cancel_pk = context.get('CANCEL')
                 bid = Bid.objects.get(pk=cancel_pk)
                 appeal_pk = bid.appeal.pk
-                bid.withdraw()
+                bid.log_event('withdraw')
+                bid.delete()
                 appeal.player.alert('appeal')
                 # mark this Appeal seen by this Player only
                 appeal.meta['seen'] = [player.pk]
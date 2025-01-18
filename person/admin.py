fbeb766a6bb5d4c6c20bd2d8e762eacdfa4e5051
nworbnhoj <john@nhoj.info>
2025-01-18 14:42:32 +1100

    refine person admin BlockAdmin

--- person/admin.py
+++ person/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-
 from .models import Block, Person, Social
 
-admin.site.register(Block)
+
+class BlockAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'person', 'blocked']
+    list_filter = ['person', 'blocked']
+
+
+admin.site.register(Block, BlockAdmin)
 admin.site.register(Person)
 admin.site.register(Social)

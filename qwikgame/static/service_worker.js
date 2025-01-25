ee448e608a9a2c9e949484e81cd6faf3db7516b6
nworbnhoj <john@nhoj.info>
2025-01-26 06:51:53 +1100

    implement PWA service worker

---
+++ qwikgame/static/service_worker.js
@@ -0,0 +1,32 @@
+///////////////// Service Worker functions ///////////////////
+
+
+// Registering Service Worker
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js');
+        console.log("serviceWorker registered.");
+    });
+};
+
+
+function getServiceWorker(){
+    if(!'serviceWorker' in navigator){
+        console.log("failed to get navigator.serviceWorker");
+        return;
+    }
+    var worker = navigator.serviceWorker.controller;  
+    if(!worker){
+        console.log("failed to get navigator.serviceWorker.controller");
+        return;
+    }
+    return worker;
+}
+
+
+function clearCache(key){
+    var worker = getServiceWorker();
+    if (worker){
+        worker.postMessage({'command': 'clearCache', 'key': key});
+    }
+}

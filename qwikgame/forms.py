41cb36d3ab3120442c7f620f64e4df961a29373b
nworbnhoj <john@nhoj.info>
2025-02-13 16:45:04 +1100

    implement qwikgame MenuForm

--- qwikgame/forms.py
+++ qwikgame/forms.py
@@ -7,3 +7,18 @@ class QwikForm(Form):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', '')  
         super().__init__(*args, **kwargs)
+
+
+class MenuForm(QwikForm):
+
+    @classmethod
+    def get(klass):
+        return { 'menu_form': klass()}
+
+    @classmethod
+    def post(klass, request_post):
+        form = klass(data=request_post)
+        context = {'menu_form': form}
+        if form.is_valid():
+            context |= {k:v for k,v in request_post.items()}
+        return context

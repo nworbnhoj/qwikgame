a27cfa8ae46894b899946e2d8e35bb207be85975
nworbnhoj <john@nhoj.info>
2025-02-21 06:50:45 +1100

    link Appeal Player icons to Player stats

--- appeal/urls.py
+++ appeal/urls.py
@@ -5,6 +5,7 @@ from appeal.views import AcceptView, BidView, AppealsView, KeenView, BidView, Ri
 urlpatterns = [
     path("", AppealsView.as_view(), name='appeal'),
     path("<int:appeal>/", BidView.as_view(), name='bid'),
+    path("<int:appeal>/<rival>/", RivalView.as_view()),
     path("keen/", KeenView.as_view(), name='keen'),
     path("keen/<str:game>/", KeenView.as_view(), name='keen'),
     path("accept/<int:appeal>/", AcceptView.as_view(), name='accept'),

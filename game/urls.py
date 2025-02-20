82a71f79cb59edd794cdb9d8f75d97ed8a96c279
nworbnhoj <john@nhoj.info>
2025-02-21 07:51:51 +1100

    link Match Player icons to Player stats

--- game/urls.py
+++ game/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from game.views import MatchView, MatchesView, ReviewView, ReviewsView
+from game.views import MatchView, MatchesView, ReviewView, ReviewsView, RivalView
 
 urlpatterns = [
     path("match/", MatchesView.as_view(), name='matches'),
     path("match/<int:match>/", MatchView.as_view(), name='match'),
+    path("match/<int:match>/<rival>/", RivalView.as_view()),
     path("match/review/", ReviewsView.as_view(), name='review'),
     path("match/review/<int:review>/", ReviewView.as_view(), name='review'),
 ]

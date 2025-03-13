nworbnhoj/qwikgame

commit a210368dabe15a6dc365e7c1655e60a86d38892c

implement Bid.announce()

appeal/views.py
@@ -243,11 +243,8 @@ class BidView(AppealsView):
                 strength=strength,
                 str_conf=confidence,
             )
-            bid.log_event('bid')
-            appeal.player.alert('appeal')
-            # mark this Appeal seen by this Player only
-            appeal.meta['seen'] = [player.pk]
-            appeal.save()
+            appeal.bid(player, bid)
+            bid.announce(player)
             # update the Mark size
             mark = Mark.objects.filter(game=appeal.game, place=appeal.venue).first()
             if mark:

appeal/models.py
@@ -249,6 +249,12 @@ class Bid(models.Model):
             expires = self.appeal.last_hour,
             context = context,            
         )
+
+    def announce(self, instigator):
+        logger.info(f'Announcing Bid: {self}')
+        self.alert('bid_new', instigator, self.appeal.player)
+        self.appeal.meta['seen'] = [instigator.pk]
+        self.log_event('bid')
     # returns the accepted datetime in venue timezone - or None otherwise
     @property
     def datetime(self):

appeal/templates/appeal/new_bid_alert_email_html.html
@@ -1,0 +1,31 @@
+<!-- templates/appeal/new_bid_alert_email_html.html -->
+{% extends "email_html.html" %}
+{% load static %}{% load i18n %}
+
+{% block title %}Qwik Alert{% endblock title %}
+
+{% block detail %}
+
+    <p style="margin: 0em; padding: 0.3em; text-align: center;">
+        {% blocktranslate %}
+            You're receiving this email because <b>{{ name }}</b> has Bid on your Qwikgame of <b>{{ game }}</b> at <b>{{venue }}</b>.
+        {% endblocktranslate %}
+    </p>
+
+    <p style="margin: 0em; padding: 0.3em; text-align: center;">
+        {{ game }}<br>
+        {{ date }}<br>
+        {{ time }}<br>
+        {{ venue }}
+    </p>
+
+    <p style="margin: 0em; padding: 0.3em; text-align: center;">
+        <a style="align-items: center; background-color: #f5a600; border: 1px solid; border-color: #f5a600; border-radius: 0.3em; color: #333333; cursor: pointer; display: inline-block; font-weight: 550;  gap: 0.3em;  margin: 0.3em; min-height: 3em; padding: 1.25em 1.5em; text-decoration: none; width: 6e;" href="http://{{ domain }}{% url 'accept' appeal=appeal.pk %} %}">
+        	{% blocktranslate %}
+                Visit {{ site_name }} to accept
+            {% endblocktranslate %}
+        </a>
+    </p>
+
+{% endblock detail %}
+

appeal/templates/appeal/new_bid_alert_email_text.html
@@ -1,0 +1,15 @@
+{% load i18n %}{% autoescape off %}
+
+{% translate "Qwik Alert" %}
+
+{% blocktranslate %}
+	You're receiving this email because {{ name }} has Bid on your Qwikgame of {{ game }} at {{venue }}.
+{% endblocktranslate %}
+
+{% translate "To accept please go to:" %}
+http://{{ domain }}{% url 'accept' appeal=appeal.pk %}
+
+{% translate "To change email notification settings, please go to:" %}
+http://{{ domain }}{% url 'private' %}
+
+{% endautoescape %}

person/models.py
@@ -102,6 +102,13 @@ class Person(models.Model):
         ):
         if self.notify_email:
             match type:
+                case 'bid_new':
+                    self.send_mail(
+                        'appeal/new_bid_alert_email_subject.txt',
+                        'appeal/new_bid_alert_email_text.html',
+                        context,
+                        'appeal/new_bid_alert_email_html.html',
+                    )
                 case 'match_chat':
                     pass
                     # self.send_mail(

commit fff3318a682efceebe3b612952a190ed8e79f82e

fix BidView.post() error introduced in a210368dabe15a6dc365e7c1655e60a86d38892c

appeal/views.py
@@ -238,7 +238,6 @@ class BidView(AppealsView):
                 strength=strength,
                 str_conf=confidence,
             )
-            appeal.bid(player, bid)
             bid.announce(player)
             # update the Mark size
             mark = Mark.objects.filter(game=appeal.game, place=appeal.venue).first()
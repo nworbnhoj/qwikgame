ce1d03dd58081c193cf0ca670e85afbb272b5e7f
nworbnhoj <john@nhoj.info>
2025-04-21 15:20:32 +1000

    refine Admin views

--- game/admin.py
+++ game/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 
 from .models import Game, Match, Review
 
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'icon']
+
 class MatchAdmin(admin.ModelAdmin):
     list_display = ['pk', 'game', 'datetime_str', 'venue', 'competitor_names']
     list_filter = [ 'game', 'date', 'venue__country', 'venue__admin1', 'venue__locality', 'venue', 'competitors' ]
@@ -13,6 +17,6 @@ class ReviewAdmin(admin.ModelAdmin):
     list_filter = ['player', 'rival', 'match']
 
 
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Match, MatchAdmin)
 admin.site.register(Review, ReviewAdmin)
--- person/admin.py
+++ person/admin.py
@@ -7,6 +7,11 @@ class BlockAdmin(admin.ModelAdmin):
     list_filter = ['person', 'blocked']
 
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'icon', 'language', 'notify_email', 'notify_push', 'user']
+    list_filter = ['icon', 'language', 'notify_email', 'notify_push']
+
+
 admin.site.register(Block, BlockAdmin)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Social)

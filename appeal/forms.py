d238426c5aebf0a7a9aa157bf4c838ede1e0dde9
nworbnhoj <john@nhoj.info>
2025-05-25 08:15:23 +1000

    Revert "auto-select singleton hour on BidForm"
    This reverts commit 555c6ccee0784ea1146fe4143df59d18cd969bf8.

--- appeal/forms.py
+++ appeal/forms.py
@@ -69,10 +69,8 @@ class BidForm(QwikForm):
     # Returns a context dict including 'rspv_form'
     @classmethod
     def get(klass, appeal):
-        hours = appeal.hour_list()
-        initial = {'hour' : hours[0]} if len(hours) == 1 else {}
-        form = klass(initial=initial)
-        form.fields['hour'].widget.set_hours_show(hours)
+        form = klass()
+        form.fields['hour'].widget.set_hours_show(appeal.hour_list())
         next_hour = 24
         if appeal.status == 'A':
             venue_now = appeal.venue.now()

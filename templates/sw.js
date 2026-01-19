b49c2cf507dae12396e05beeac5d463f94b3fb2e
nworbnhoj <john@nhoj.info>
2026-01-19 22:46:17 +1000

    relocate sw appSheel cach into workbox block

--- templates/sw.js
+++ templates/sw.js
@@ -23,55 +23,58 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox
 
 if (workbox) {
   console.log(`Workbox loaded 🎉`);
+
+  const appShell = [
+      '{{ css_all_min_url }}',
+      '{{ css_map_url }}',
+      '{{ css_qwik_url }}',
+      '{{ css_reset_url }}',
+      '{{ css_small_screen_url }}',
+      '{{ css_welcome_url }}',
+      '{{ favicon_url }}',
+      '{{ font_astrospace_url }}',
+      '{{ font_fa_brands_url }}',
+      '{{ font_fa_regular_url }}',
+      '{{ font_fa_solid_url }}',
+      '{{ font_notosans_url }}',
+      '{{ icon_url }}',
+      '{{ icon_152_url }}',
+      '{{ icon_192_url }}',
+      '{{ icon_512_url }}',
+      '{{ img_creativecommons_url }}',
+      '{{ img_ss_feed_chat_url }}',
+      '{{ img_ss_home_url }}',
+      '{{ img_ss_invite_offer_url }}',
+      '{{ js_map_url }}',
+      '{{ js_qwik_url }}',
+      '{{ js_qwik_json_url }}',
+      '{{ js_qwik_pwa_install_url }}',
+      '{{ js_qwik_sw_load_url }}',
+      '{{ logo_url }}',
+      '{{ logo_152_url }}',
+      '{{ logo_192_url }}',
+      '{{ logo_512_url }}',
+      '{{ manifest_url }}',
+      '{{ mp3_chime_url }}',
+       '{{ welcome_url }}',
+      // '{{ offline_url }}',
+  ].map((partialUrl) => `${location.protocol}//${location.host}${partialUrl}`);
+
+  // Precache the shell.
+  workbox.precaching.precacheAndRoute(appShell.map(url => ({
+      url,
+      revision: VERSION,
+  })));
+
+  // Serve the app shell from the cache.
+  workbox.routing.registerRoute(
+    ({url}) => appShell.includes(url),
+    new workbox.strategies.CacheOnly()
+  );
 } else {
   console.log(`Warning: Workbox didn't load 😬`);
 }
 
-
-const appShell = [
-    '{{ css_all_min_url }}',
-    '{{ css_map_url }}',
-    '{{ css_qwik_url }}',
-    '{{ css_reset_url }}',
-    '{{ css_small_screen_url }}',
-    '{{ css_welcome_url }}',
-    '{{ favicon_url }}',
-    '{{ font_astrospace_url }}',
-    '{{ font_fa_brands_url }}',
-    '{{ font_fa_regular_url }}',
-    '{{ font_fa_solid_url }}',
-    '{{ font_notosans_url }}',
-    '{{ icon_url }}',
-    '{{ icon_152_url }}',
-    '{{ icon_192_url }}',
-    '{{ icon_512_url }}',
-    '{{ img_creativecommons_url }}',
-    '{{ img_ss_feed_chat_url }}',
-    '{{ img_ss_home_url }}',
-    '{{ img_ss_invite_offer_url }}',
-    '{{ js_map_url }}',
-    '{{ js_qwik_url }}',
-    '{{ js_qwik_json_url }}',
-    '{{ js_qwik_pwa_install_url }}',
-    '{{ js_qwik_sw_load_url }}',
-    '{{ logo_url }}',
-    '{{ logo_152_url }}',
-    '{{ logo_192_url }}',
-    '{{ logo_512_url }}',
-    '{{ manifest_url }}',
-    '{{ mp3_chime_url }}',
-     '{{ welcome_url }}',
-    // '{{ offline_url }}',
-].map((partialUrl) => `${location.protocol}//${location.host}${partialUrl}`);
-
-// Precache the shell.
-workbox.precaching.precacheAndRoute(appShell.map(url => ({
-    url,
-    revision: VERSION,
-})));
-
-// Serve the app shell from the cache.
-workbox.routing.registerRoute(({url}) => appShell.includes(url), new workbox.strategies.CacheOnly());
 
 
 

e5b990a5a5427518a2b6a03616943093ed713744
nworbnhoj <john@nhoj.info>
2026-02-25 04:45:34 +1100

    add LANGUAGES and LOCALE_PATHS to settings

--- qwikgame/settings.py
+++ qwikgame/settings.py
@@ -169,6 +169,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+    ("en", "English"),
+    ("bg", "Bulgarian"),
+]
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/

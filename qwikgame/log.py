b90162e594a8259916cd2acaaa087b0b63f32038
nworbnhoj <john@nhoj.info>
2026-03-19 09:08:49 +1100

    implement log Entry.player

--- qwikgame/log.py
+++ qwikgame/log.py
@@ -5,10 +5,13 @@ class Entry(dict):
     def __init__(self, id, hash=None, klass=None, name=None, pk=None, text=None):
         dict.__init__(self, id=id, created=now().strftime("%Y-%m-%d %H:%M:%S%z"), hash=hash, klass=klass, name=name, pk=pk, text=text)
 
+    @property
+    def player(self):
+        if id in self:
+            return player.models.Player.objects.filter(pk=self['id']).first();
 
     def rename(self, player):
-        if id in self:
-            rival = player.models.Player.objects.filter(pk=self['id']).first();
-            if rival:
-                self['name'] = player.name_rival(rival);
+        rival = self.player
+        if rival:
+            self['name'] = player.name_rival(rival);
 

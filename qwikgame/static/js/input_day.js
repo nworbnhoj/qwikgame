a574482031a9847bfd810ae20c67ee125acbdc39
nworbnhoj <john@nhoj.info>
2026-06-09 08:31:41 +1000

    re-implement input_day.js

--- qwikgame/static/js/input_day.js
+++ qwikgame/static/js/input_day.js
@@ -4,8 +4,8 @@ if (typeof toggleAllDay == "undefined") {
 
   // update an AllDay toggle to be consistent with the Hour checkboxes
   function updateAllDay(all_day) {
-    if (!all_day) {
-      console.warn("updateAllDay(null)")
+    if (!all_day || all_day.tagName !== 'LABEL') {
+      console.warn("updateAllDay() called with " + all_day);
       return;
     }
     if ('updatePromise' in all_day) {
@@ -13,67 +13,52 @@ if (typeof toggleAllDay == "undefined") {
     }
     all_day.updatePromise = new Promise((resolve, reject) => {
       try {
-        // check all_day if every hour is checked
-        let hours = all_day.closest('div.by_day').querySelector('div.radio_block').children;
-        for (hour of hours) {
-          var checkbox = hour.firstElementChild;
-          if (!checkbox.disabled && !checkbox.checked) {
-            resolve(false);
-          }
+        const FIELD = all_day.closest('div.field');
+        // uncheck all_day if a single hour is unchecked
+        if (FIELD.querySelector("label.hour input[type='checkbox']:not(:checked):not(:disabled)")){
+          toggle_uncheck(all_day);
+          resolve(false);
+        } else {
+          toggle_check(all_day);
+          resolve(true);
         }
-        toggle_check(all_day);
-        resolve(true);
       } catch (e) {
         console.log(e);
       }
     });
     all_day.updatePromise.then(() => { delete all_day.updatePromise });
-      return all_day.updatePromise;
+    return all_day.updatePromise;
   }
 
-  // promote a change in an Hour checkbox up to the AllDay toggle
-  function boggleHour(event) {
-    try {
-      let hour = event.currentTarget;
-      let all_day = hour.closest(".by_day").querySelector("label.toggle.all_day");
-      if (toggle_checked(hour)) {
-        updateAllDay(all_day);
-      } else {
-        toggle_uncheck(all_day);
-        let all_week = all_day.closest('div.field').querySelector("label.toggle.all_week");
-        toggle_uncheck(all_week);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  // cascade a change in the AllDay toggle to the Hour checkboxes
+  // cascade a change in the AllDay toggle DOWN to the Hour checkboxes
   function toggleAllDay(event) {
     try {
-      let all_day = event.currentTarget;
-      let all_day_checked = toggle_checked(all_day);
-      var hours = all_day.closest('div.by_day').querySelector('div.radio_block').children;
-      for (hour of hours) {
-        var checkbox = hour.firstElementChild;
-        if (!checkbox.disabled) {
-          checkbox.checked = all_day_checked;
-        }
-      }
+      const ALL_DAY = event.currentTarget;
+      const ALL_DAY_CHECKED = toggle_checked(ALL_DAY);
+      const ON_DAY = ALL_DAY.closest('div.on_day');
+      const BY_HOUR = ON_DAY.querySelector('div.by_hour');
+      BY_HOUR.querySelectorAll("input[type='checkbox']:not(:disabled)").forEach((hour) => {
+        hour.checked = ALL_DAY_CHECKED;
+      });
     } catch (e) {
       console.log(e);
     }
   }
 
-  window.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('label.toggle.all_day').forEach(function(all_day) {
-      updateAllDay(all_day)
       all_day.addEventListener("click", toggleAllDay);
-      the_day = all_day.parentElement.nextElementSibling;
-      the_day.querySelectorAll('label.toggle.hour').forEach(function(hour) {
-        hour.addEventListener("click", boggleHour);
+      on_day = all_day.closest('.on_day');
+      // propogate a click on an Hour toggle up to the AllDay toggle
+      on_day.querySelectorAll('label.toggle.hour').forEach(function(hour) {
+        hour.addEventListener("click", () => { updateAllDay(all_day); });
       });
     });
   });
 
+  window.addEventListener("load", function() {
+    document.querySelectorAll('label.toggle.all_day').forEach(function(all_day) {
+      updateAllDay(all_day)
+    });
+  });
 }

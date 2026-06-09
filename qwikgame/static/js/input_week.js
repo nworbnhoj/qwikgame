69ee40816d3241b7f842fc0bcdbf34c28e3df5fa
nworbnhoj <john@nhoj.info>
2026-06-09 10:49:30 +1000

    re-implement input_week.js

--- qwikgame/static/js/input_week.js
+++ qwikgame/static/js/input_week.js
@@ -4,8 +4,8 @@ if (typeof toggleAllWeek == "undefined") {
 
   // update an AllWeek toggle to be consistent with the AllDay toggles
   function updateAllWeek(all_week) {
-    if (!all_week) {
-      console.warn("updateAllWeek(null)")
+    if (!all_week || all_week.tagName !== 'LABEL') {
+      console.warn("updateAllWeek() called with " + all_week);
       return;
     }
     if ('updatePromise' in all_week) {
@@ -13,15 +13,15 @@ if (typeof toggleAllWeek == "undefined") {
     }
     all_week.updatePromise = new Promise((resolve, reject) => {
       try {
-        // check all_week if every all_day is checked
-        let days = all_week.closest('div.field').querySelectorAll("label.toggle.all_day");
-        for (day of days) {
-          if (!toggle_disabled(day) && !toggle_checked(day)) {
-            resolve(false);
-          }
+        const FIELD = all_week.closest('div.field');
+        // uncheck all_day if a single hour is unchecked
+        if (FIELD.querySelector("label.all_day input[type='checkbox']:not(:checked):not(:disabled)")){
+          toggle_uncheck(all_week);
+          resolve(false);
+        } else {
+          toggle_check(all_week);
+          resolve(true);
         }
-        toggle_check(all_week);
-        resolve(true);
       } catch (e) {
         console.log(e);
         reject(e);
@@ -31,74 +31,52 @@ if (typeof toggleAllWeek == "undefined") {
     return all_week.updatePromise;
   }
 
-  // promote a change in an Hour up to the AllWeek toggle
-  function hoggleHour(event) {
-    try {
-      let hour = event.currentTarget;
-      let all_day = hour.closest(".by_day").querySelector("label.toggle.all_day");
-      if (toggle_checked(hour)) {
-        updateAllDay(all_day);
-        let all_week = all_day.closest('div.field').querySelector("label.toggle.all_week");
-        if (toggle_checked(all_day)){
-          updateAllWeek(all_week);
-        } else {
-          toggle_uncheck(all_week);
-        }
-      } else {
-        toggle_uncheck(all_day);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  // promote a change in an AllDay checkbox up to the AllWeek toggle
-  function boggleAllDay(event) {
-    try {
-      let all_day = event.currentTarget;
-      let all_week = all_day.closest('div.field').querySelector("label.toggle.all_week");
-      if (toggle_checked(all_day)) {
-        updateAllWeek(all_week);
-      } else {
-        toggle_uncheck(all_week);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  // cascade a change in the AllWeek toggle to the AllDay toggles
+  // cascade a change in the AllWeek toggle DOWN to the AllDay toggles
   function toggleAllWeek(event) {
     try {
-      let button = event.currentTarget;
-      var checked = button.firstElementChild.checked;
-      let detail = button.closest('div.field');
-      detail.querySelectorAll('.all_day').forEach(function(button) {
-        button.firstElementChild.checked = checked;
-      })
-      detail.querySelectorAll('div.hour_grid').forEach(function(radio_block) {
-        for (hour of radio_block.children) {
-          hour.firstElementChild.checked = checked;
-        }
-      })
+      const ALL_WEEK = event.currentTarget;
+      const ALL_WEEK_CHECKED = toggle_checked(ALL_WEEK);
+      const FIELD = ALL_WEEK.closest('div.field');
+      FIELD.querySelectorAll("input[type='checkbox']:not(:disabled)").forEach((day) => {
+        day.checked = ALL_WEEK_CHECKED;
+      });
     } catch (e) {
       console.log(e);
     }
   }
 
-  window.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('label.toggle.all_week').forEach(function(all_week) {
       all_week.addEventListener("click", toggleAllWeek);
-      the_week = all_week.closest('.by_week').nextElementSibling;
-      the_week.querySelectorAll('label.toggle.all_day').forEach(function(all_day) {
-        all_day.addEventListener("click", boggleAllDay);
-        updateAllDay(all_day);
+      field = all_week.closest('div.field');
+      // propagate a click on the AllDay toggle up to the AllWeek toggle
+      field.querySelectorAll('label.toggle.all_day').forEach(function(all_day) {
+        all_day.addEventListener("click", () => {
+          updateAllWeek(all_week);
+        });
       });
-      all_hours = the_week.querySelectorAll('label.toggle.hour').forEach(function(hour) {
-        hour.addEventListener('click', hoggleHour);
+      // propagate a click on an Hour toggle up to the AllWeek toggle
+      field.querySelectorAll('.by_day').forEach(function(by_day) {
+        let all_day = by_day.querySelector('label.toggle.all_day');
+        by_day.querySelectorAll('label.toggle.hour').forEach(function(hour) {
+          hour.addEventListener("click", () => {
+            updateAllDay(all_day).then(updateAllWeek(all_week));
+          });
+        });      
       });
-      updateAllWeek(all_week)
     });
   });
 
+  window.addEventListener("load", function() {
+    document.querySelectorAll('label.toggle.all_week').forEach(function(all_week) {
+      const FIELD = all_week.closest('div.field');
+      let allDayPromises = [];
+      FIELD.querySelectorAll('label.toggle.all_day').forEach(function(all_day) {
+        allDayPromises.push(updateAllDay(all_day));
+      });
+      Promise.all(allDayPromises).then((values) => {
+        updateAllWeek(all_week)
+      });
+    });
+  });
 }
